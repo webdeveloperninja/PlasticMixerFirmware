c7e0f5472a65789de6daf14c46a25e6a0d6ee451
Robert Smith <rsmith5901@gmail.com>
2024-04-13 22:21:58 -0400

    closer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -54,13 +54,19 @@ osThreadId defaultTaskHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 void StartDefaultTask(void const * argument);
-
 
 /* USER CODE BEGIN PFP */
 
-void StartAdditiveMaterialMotorClockwise(int stepsPerSecond);
+void StartAdditiveMaterialMotorClockwise();
 void StopAdditiveMaterialMotor();
+void CalibrateVirginMaterialFlow();
+void SetCalibrationValveOpen();
+void SetCalibrationValveClosed();
+void StartFeed();
+void StopFeed();
+void StartAdditiveMotorTask(void *argument);
 
 /* USER CODE END PFP */
 
@@ -69,13 +75,13 @@ void StopAdditiveMaterialMotor();
 
 
 // SET FROM CALIBRATION PROCESS
-int VirginMaterialCupsPerSecond = 0;
+int VirginMaterialGramsPerSecond = 0;
 
 // USER INPUT FROM HMI
-int VirginMaterialGramsPerCup = 0;
+int VirginMaterialGramsPerCup = 30;
 
 // USER INPUT FROM HMI
-int PercentColorMaterial = 0;
+int AdditivePercentRate = 2;
 
 // SET FROM CALIBRATION
 int AdditiveMaterialMotorStepsPerSecond = 100;
@@ -89,23 +95,9 @@ int CalibrationValveOpen = FALSE;
 int CalibrationTubeFull = FALSE;
 
 
+// TODO Interrupt Set CalibrationTubeFull based on limit switch
+
 TaskHandle_t xStartAdditiveMotorTaskHandle = NULL;
-
-void StartAdditiveMotorTask(void *argument)
-{
-    const TickType_t xDelay = CalculateAdditiveMaterialMotorSpeedIntervalSeconds * 1000 / portTICK_PERIOD_MS;
-
-    while (1) {
-        // Run Long process to determine VirginMaterialCupsPerSecond
-
-    	// Calculate AdditiveMaterialMotorStepsPerSecond
-
-        StartAdditiveMaterialMotorClockwise(AdditiveMaterialMotorStepsPerSecond);
-
-        printf("Virgin Material Flow Calibration Task\n");
-        vTaskDelay(xDelay);
-    }
-}
 
 void StartFeed()
 {
@@ -133,7 +125,45 @@ void StopFeed()
 	}
 }
 
-void StartAdditiveMaterialMotorClockwise(int stepsPerSecond)
+void StartAdditiveMotorTask(void *argument)
+{
+    const TickType_t calculateAdditiveMaterialMotorSpeedInterval = CalculateAdditiveMaterialMotorSpeedIntervalSeconds * 1000 / portTICK_PERIOD_MS;
+
+    while (1)
+    {
+        CalibrateVirginMaterialFlow();
+        StartAdditiveMaterialMotorClockwise();
+
+        printf("Virgin Material Flow Calibration Task\n");
+        vTaskDelay(calculateAdditiveMaterialMotorSpeedInterval);
+    }
+}
+
+void CalibrateVirginMaterialFlow() {
+    TickType_t xStartTime, xEndTime;
+    int timeToFillMilliseconds;
+
+    SetCalibrationValveOpen();
+
+    xStartTime = xTaskGetTickCount();
+
+    while (CalibrationTubeFull == FALSE) {
+    	// DELAY Polling Calibration Tube Full to save CPU cycles
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+
+    xEndTime = xTaskGetTickCount();
+
+    SetCalibrationValveClosed();
+
+    timeToFillMilliseconds = (xEndTime - xStartTime) * portTICK_PERIOD_MS;
+    VirginMaterialGramsPerSecond = (float) VirginMaterialGramsPerCup / (timeToFillMilliseconds / 1000.0f);
+
+    printf("Virgin Material Flow Rate: %d grams/second\n", VirginMaterialGramsPerSecond);
+}
+
+
+void StartAdditiveMaterialMotorClockwise()
 {
 	// TODO: Implement
 }
@@ -144,6 +174,35 @@ void StopAdditiveMaterialMotor()
 	// TODO: Implement
 }
 
+void SetCalibrationValveOpen()
+{
+	// TODO: Implement
+	CalibrationValveOpen = TRUE;
+}
+
+void SetCalibrationValveClosed()
+{
+	// TODO: Implement
+	CalibrationValveOpen = FALSE;
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if (GPIO_Pin == GPIO_PIN_13)
+    {
+        if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET)
+        {
+            CalibrationTubeFull = TRUE;
+            printf("Button Pressed - CalibrationTubeFull set to TRUE\n");
+        }
+        else // Pin is low (button released)
+        {
+            CalibrationTubeFull = FALSE;
+            printf("Button Released - CalibrationTubeFull set to FALSE\n");
+        }
+    }
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -152,16 +211,76 @@ void StopAdditiveMaterialMotor()
   */
 int main(void)
 {
-  HAL_Init();                      // Initialize the Hardware Abstraction Layer
-  SystemClock_Config();            // Configure the System Clock
 
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+
+  // TODO: This will move to an interrupt triggering on HMI Feed button
   StartFeed();
 
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
   /* Start scheduler */
-  vTaskStartScheduler();           // Start the FreeRTOS scheduler
+  osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
-  for (;;);
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
 /**
@@ -203,6 +322,34 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
